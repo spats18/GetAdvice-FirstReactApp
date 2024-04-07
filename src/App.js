1f5926da5813cbcf668529dccfb9677e9463292b
Sannidhya Pathania <pathaniasannidhya@gmail.com>
2024-04-07 13:31:58 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {useEffect, useState} from "react";
+function App(){
+    const [advice, setAdvice] = useState("Click on the button to get an advice");
+    // setAdvice is the setter function that we use to update the value of state i.e. advice
+    const [count, setCount] = useState(0);
+    async function getAdvice(){
+        const res = await fetch("https://api.adviceslip.com/advice");
+        const data = await res.json();
+        setAdvice(data.slip.advice);
+        setCount(c => c+1);
+        // whenever the getAdvice is called, setAdvice will update the value of advice
+    }
+
+    useEffect(function () {
+        getAdvice()
+    }, []);
+    return (
+        <div>
+        {/* to run any javascript code inside html tags*/}
+            <h1>{advice}</h1>
+            <button onClick={getAdvice}>Get advice</button>
+            <Message count={count}/>
+        {/* Adding it like an HTML component*/}
+        </div>
+    );
+}
+
+function Message(props) {
+    return (<p>
+        You have read <strong>{props.count}</strong> pieces of advice
+    </p>);
 }
 
 export default App;
